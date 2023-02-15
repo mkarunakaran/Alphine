71973208ae6821986fa81170f6c25dfad7423f7d
Murali <murali.karunakaran@gmail.com>
2023-02-14 21:55:15 -0500

    Add files via upload

---
+++ Accounts/GetSharedHolderProfileQuery.cs
@@ -0,0 +1,14 @@
+﻿using Sumday.Domain.Abstractions.EntryPorts;
+
+namespace Sumday.BoundedContext.ShareHolder
+{
+    public class GetSharedHolderProfileQuery : IQueryInputPort
+    {
+        public GetSharedHolderProfileQuery(string sharedHolderId)
+        {
+            this.SharedHolderId = sharedHolderId;
+        }
+
+        public string SharedHolderId { get; }
+    }
+}
